80430a82339e1e8908401859f921d2d4cab53115
DimGj <91105160+DimGj@users.noreply.github.com>
2022-04-17 23:06:54 +0300

    Update main.py

--- code/main.py
+++ code/main.py
@@ -7,6 +7,7 @@ import pickle
 import pandas as pd
 import nltk
 from nltk.corpus import stopwords
+import re
 import csv
 
 def LoadTweets():
@@ -17,22 +18,29 @@ def LoadTweets():
             unpickler = pickle.Unpickler(f)
             Data = unpickler.load()
             df  = pd.DataFrame(Data)
+            f.close()
             return df
     else:
-      print("Error ,tweets.pkl not found!")
-      exit()
+        print("Error ,tweets.pkl not found!")
+        exit()
 
 def CleanTweets():
 
-  Data = LoadTweets()
-  #Data["user_description"] = Data['user_description'].transform(lambda x: x.lower() if isinstance(x,str) else x)
- # Data.transform(lambda x: x.lower() if isinstance(x,str) else x)
-  Data = Data.astype(str).apply(lambda x: x.str.encode('ascii', 'ignore').str.decode('ascii'))
-  for columns in Data.columns:
-    Data[columns] = Data[columns].str.lower()
-  #tokenizer = nltk.RegexpTokenizer(r"\w+")
-  #User_Description = tokenizer.tokenize(User_Description)
-  print(Data)
+    Data = LoadTweets()
+    #Data["user_description"] = Data['user_description'].transform(lambda x: x.lower() if isinstance(x,str) else x)
+    #Data.transform(lambda x: x.lower() if isinstance(x,str) else x)
+    Data = Data.astype(str).apply(lambda x: x.str.encode('ascii', 'ignore').str.decode('ascii'))
+    Data['text'] = Data['text'].str.lower()
+    #tokenizer = nltk.RegexpTokenizer(r"\w+")
+    #User_Description = tokenizer.tokenize(User_Description)
+    #re.sub(combined_pat, '', Data)
+    cleaned = []
+    elements = list(Data['text'])
+    for i in elements:
+        cleanedText = re.sub("[^a-zA-Z0-9]", " ",i)
+        cleaned.append(re.sub(r'^RT[\s]+', '', cleanedText))
+    Data['text'] = cleaned
+    print(Data['text'])
 
 
 CleanTweets()
