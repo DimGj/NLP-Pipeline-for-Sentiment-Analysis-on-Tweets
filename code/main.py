237dfa9ca0b90e4a4d9fe12cad3072b9c8d6772f
natedrake7 <92892346+natedrake7@users.noreply.github.com>
2022-04-17 19:59:32 +0300

    Add files via upload

--- code/main.py
+++ code/main.py
@@ -1,22 +1,22 @@
 import os
-import numpy
-from ossaudiodev import SNDCTL_COPR_RESET
+import numpy as np
 import pickle
-import pandas
+import pandas as pd
 import nltk
 import csv
 
 def LoadTweets():
-    Scores = {}
+    Data = {}
     Target = "tweets.pkl"
     if os.path.getsize(Target) > 0 :
         with open(Target,"rb") as f:
             unpickler = pickle.Unpickler(f)
-            Scores = unpickler.load()
-            dict((k.lower(), v.lower()) for k,v in Scores.iteritems())
-            for i in Scores:
-                print(i,Scores[i])
-
+            Data = unpickler.load()
+           # print(Data)
+            df  = pd.DataFrame(Data)
+          #  df.astype(str).apply(lambda x: x.str.encode('ascii', 'ignore').str.decode('ascii'))
+            User_Description = df['user_description'].map(lambda x: x.lower() if isinstance(x,str) else x)
+            print(User_Description)
     else:
         print("Error,file not found")
 LoadTweets()
