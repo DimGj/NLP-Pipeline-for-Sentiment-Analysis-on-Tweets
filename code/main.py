9d4aed674b2d689a1a1ef4c097aeccdccfb134c0
natedrake7 <92892346+natedrake7@users.noreply.github.com>
2022-04-21 22:19:45 +0300

    Add files via upload

--- code/main.py
+++ code/main.py
@@ -23,7 +23,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 def LoadTweets():
     Data = {}
-    Target = "tweets.pkl"
+    Target = "../tweets/tweets.pkl"
     if exists(Target):
     	with open(Target,"rb") as f:
             unpickler = pickle.Unpickler(f)
@@ -68,8 +68,26 @@ def SplitDataFrame():
     Test.to_csv(test_path, sep='\t', index=False)
 
 def BagOfWords():
-    Train = OpenFile('Train.tsv')
-    print(Train['text'])
+    from sklearn.feature_extraction.text import CountVectorizer
+    vectorizer = CountVectorizer(max_df=1.0, min_df=1, max_features=1000,
+    stop_words='english')
+    vector = vectorizer.fit_transform(Data['text'])
+    df_bow_sklearn = pd.DataFrame(vector.toarray(),columns=vectorizer.get_feature_names_out())
+    SaveFile(df_bow_sklearn,'bagofwords.pkl')
+
+def TF_IDF():
+   from sklearn.feature_extraction.text import CountVectorizer
+   from sklearn.feature_extraction.text import TfidfVectorizer
+   tfidf_vectorizer = TfidfVectorizer(max_df=1.0, min_df=1, max_features=1000,
+    stop_words='english')
+   tfidf_vector = tfidf_vectorizer.fit_transform(Data['text'])
+   tfidf_df = pd.DataFrame(tfidf_vector.toarray(),columns=tfidf_vectorizer.get_feature_names_out())
+   SaveFile(tfidf_df,'TF-IDF.pkl')
+
+def SaveFile(DataFrame,Filename):
+    if not exists(Filename):
+        with open(Filename,'wb') as handle:
+            pickle.dump(DataFrame,handle,protocol=pickle.HIGHEST_PROTOCOL)
 
 def OpenFile(Filename):
     if exists(Filename):
@@ -94,7 +112,8 @@ def PrintDataFrame():
     for items in Data:
         print(items)
 
+TF_IDF()
 #BagOfWords()
-PrintDataFrame()
+#PrintDataFrame()
 #DataAnalysis_v()
 #SplitDataFrame()
