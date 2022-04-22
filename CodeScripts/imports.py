03b91452d784ddca11363cc0eb0f0cbeb3bc6405
DimGj <91105160+DimGj@users.noreply.github.com>
2022-04-22 20:01:29 +0300

    Add files via upload

---
+++ CodeScripts/imports.py
@@ -0,0 +1,20 @@
+from ast import Load
+from email import header
+from fileinput import filename
+from genericpath import exists
+from operator import pos, truediv
+from pathlib import Path
+import sys
+import os
+from tkinter import W
+from turtle import color, title
+import numpy as np
+import pickle
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+import re
+import csv
+import sklearn.model_selection
+from collections import Counter
+from matplotlib import pyplot as plt
---
+++ CodeScripts/main.py
@@ -0,0 +1,36 @@
+import imports
+import FileHandlers
+import VectorizationTweets
+import DataAnalysis
+
+def CleanTweets():
+
+    Data = FileHandlers.LoadFile("../PickleFiles/tweets.pkl")
+    Data = Data.astype(str).apply(lambda x: x.str.encode('ascii', 'ignore').str.decode('ascii'))
+    Data['text'] = Data['text'].str.lower()
+    stop  = imports.stopwords.words('english')
+    pat = r'\b(?:{})\b'.format('|'.join(stop))
+    Data['text'] = Data['text'].str.replace(pat, '',regex = True)
+    Data['text'] = Data['text'].str.replace(r'\s+', ' ',regex = True)
+    cleaned = []
+    elements = list(Data['text'])
+    for i in elements:
+        FlteredText = imports.re.sub('https?://[A-Za-z0-9./]+','',i)
+        FlteredText = imports.re.sub("[^a-zA-Z0-9]", " ",FlteredText)
+        cleaned.append(imports.re.sub(r'^RT[\s]+', '', FlteredText))
+    Data['text'] = cleaned
+    return Data
+
+def PrintDataFrame():
+    print(Data)
+
+Data = CleanTweets()
+#VectorizationTweets.TF_IDF(Data)
+#VectorizationTweets.BagOfWords(Data)
+#PrintDataFrame()
+#DataAnalysis.DataAnalysis_i(Data)
+#DataAnalysis.DataAnalysis_ii(Data)
+#DataAnalysis.DataAnalysis_iii(Data)
+#DataAnalysis.DataAnalysis_iv(Data)
+#DataAnalysis.DataAnalysis_v(Data)
+FileHandlers.SplitDataFrame(Data)
