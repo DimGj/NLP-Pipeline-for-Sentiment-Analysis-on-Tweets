9b484c78808aba5eedb8934a337366d4f024a7ee
DimGj <91105160+DimGj@users.noreply.github.com>
2022-04-22 18:49:57 +0300

    Update VectorizationTweets.py

--- code/VectorizationTweets.py
+++ code/VectorizationTweets.py
@@ -1,12 +1,12 @@
 import main
-import pandas as pd
 
 def BagOfWords():
+    Data =  main.CleanTweets()
     from sklearn.feature_extraction.text import CountVectorizer
     vectorizer = CountVectorizer(max_df=1.0, min_df=1, max_features=1000,
     stop_words='english')
     vector = vectorizer.fit_transform(Data['text'])
-    df_bow_sklearn = pd.DataFrame(vector.toarray(),columns=vectorizer.get_feature_names_out())
+    df_bow_sklearn = main.pd.DataFrame(vector.toarray(),columns=vectorizer.get_feature_names_out())
     SaveFile(df_bow_sklearn,'bagofwords.pkl')
 
 def TF_IDF():
@@ -16,4 +16,4 @@ def TF_IDF():
     stop_words='english')
    tfidf_vector = tfidf_vectorizer.fit_transform(Data['text'])
    tfidf_df = pd.DataFrame(tfidf_vector.toarray(),columns=tfidf_vectorizer.get_feature_names_out())
-   SaveFile(tfidf_df,'TF-IDF.pkl')
+   main.SaveFile(tfidf_df,'TF-IDF.pkl')
