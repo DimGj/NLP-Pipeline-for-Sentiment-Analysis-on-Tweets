256471fcbefce2877694d93f4b11876cc6eef76b
natedrake7 <92892346+natedrake7@users.noreply.github.com>
2022-04-30 20:17:40 +0300

    Update Classification.py

--- CodeScripts/Classification.py
+++ CodeScripts/Classification.py
@@ -1,16 +1,24 @@
-from matplotlib.pyplot import clf
+from sklearn import datasets
 from VectorizationTweets import TF_IDF
 import imports
 import FileHandlers
 
 def SVM():
+    Train = FileHandlers.OpenFile('../TSVFiles/Train.tsv')
+    Test = FileHandlers.OpenFile('../TSVFiles/Test.tsv')
     BOWTrain = FileHandlers.LoadFile('../PickleFiles/bagofwordsTrain.pkl')
     BOWTest = FileHandlers.LoadFile('../PickleFiles/bagofwordsTest.pkl')
-   # TF_IDF = FileHandlers.LoadFile('../PickleFiles/TF-IDFTrain.pkl')
-   # WE = FileHandlers.LoadFile('../PickleFiles/WordEmbeddingsTrain.pkl')
-    clf = imports.svm.SVC(gamma=0.001,C=100.)
-    print(BOWTrain)
-   # clf.fit(BOWTrain,BowTrainSen)
-   # y_pred = clf.predict(BOWTest)
-   # accuracy = imports.accuracy_score(BOWTest,y_pred)*100
-   # print(accuracy)
+    clf = imports.svm.SVC()
+    clf.fit(BOWTrain[:20000],Train['sentiment'][:20000])
+    ScoresBOW = imports.cross_val_score(clf, BOWTest[:10000], Test['sentiment'][:10000], scoring='accuracy',cv=10, n_jobs=-1)
+    print("Scores for the Bag Of Words are: ", ScoresBOW)
+    TFIDTrain = FileHandlers.LoadFile('../PickleFiles/TF-IDFTrain.pkl')
+    TFIDTest = FileHandlers.LoadFile('../PickleFiles/TF-IDFTest.pkl')
+    clf.fit(TFIDTrain[:20000],Train['sentiment'][:20000])
+    ScoresTFIDF = imports.cross_val_score(clf, TFIDTest[:10000], Test['sentiment'][:10000], scoring='accuracy',cv=10, n_jobs=-1)
+    print("Scores for the TF-IDF are: ", ScoresTFIDF)
+    #WETrain = FileHandlers.LoadFile('../PickleFiles/WordEmbeddingsTrain.pkl')
+    #WETest = FileHandlers.LoadFile('../PickleFiles/WordEmbeddingsTest.pkl')
+    #clf.fit(List[:1000],Train['sentiment'][:1000])
+    #ScoresWE = imports.cross_val_score(clf, WETest[:500], Test['sentiment'][:500], scoring='accuracy',cv=10, n_jobs=-1)
+    #print("Scores for the Word Embeddings are: ", ScoresWE)
