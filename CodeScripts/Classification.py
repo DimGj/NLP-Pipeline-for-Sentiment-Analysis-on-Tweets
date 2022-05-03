ea862520bfda42eb41fa3ccc4614a42429cc0c64
natedrake7 <92892346+natedrake7@users.noreply.github.com>
2022-05-04 01:12:50 +0300

    Add files via upload

--- CodeScripts/Classification.py
+++ CodeScripts/Classification.py
@@ -1,3 +1,5 @@
+from math import gamma
+from re import T
 from sklearn import datasets
 from VectorizationTweets import TF_IDF
 import imports
@@ -8,20 +10,21 @@ def SVM():
     Test = FileHandlers.OpenFile('../TSVFiles/Test.tsv')
     BOWTrain = FileHandlers.LoadFile('../PickleFiles/bagofwordsTrain.pkl')
     BOWTest = FileHandlers.LoadFile('../PickleFiles/bagofwordsTest.pkl')
-    clf = imports.svm.LinearSVC()
-    clf.fit(BOWTrain[:20000],Train['sentiment'][:20000])
-    ScoresBOW = imports.cross_val_score(clf, BOWTest[:10000], Test['sentiment'][:10000], scoring='accuracy',cv=10, n_jobs=-1)
+    clf = imports.svm.LinearSVC(C=1.0,dual=False)
+    clf.fit(BOWTrain,Train['sentiment'])
+    ScoresBOW = imports.cross_val_score(clf, BOWTest,Test['sentiment'], scoring='accuracy',cv=10, n_jobs=-1)
     print("Scores for the Bag Of Words are: ", ScoresBOW)
     TFIDTrain = FileHandlers.LoadFile('../PickleFiles/TF-IDFTrain.pkl')
     TFIDTest = FileHandlers.LoadFile('../PickleFiles/TF-IDFTest.pkl')
-    clf.fit(TFIDTrain[:20000],Train['sentiment'][:20000])
-    ScoresTFIDF = imports.cross_val_score(clf, TFIDTest[:10000], Test['sentiment'][:10000], scoring='accuracy',cv=10, n_jobs=-1)
+    clf.fit(TFIDTrain,Train['sentiment'])
+    ScoresTFIDF = imports.cross_val_score(clf, TFIDTest, Test['sentiment'], scoring='accuracy',cv=10, n_jobs=-1)
     print("Scores for the TF-IDF are: ", ScoresTFIDF)
 
 def SVM2():
     WETrain = FileHandlers.LoadFile('../PickleFiles/WordEmbeddingsTrain.pkl')
     WETest = FileHandlers.LoadFile('../PickleFiles/WordEmbeddingsTest.pkl')
-    print(WETrain.wv)
+    words = list(WETrain.wv.key_to_index)
+    print(words)
     #clf = imports.svm.SVC()
     #clf.fit(List[:1000],Train['sentiment'][:1000])
     #ScoresWE = imports.cross_val_score(clf, WETest[:500], Test['sentiment'][:500], scoring='accuracy',cv=10, n_jobs=-1)
@@ -32,10 +35,10 @@ def RandomForest():
     Test = FileHandlers.OpenFile('../TSVFiles/Test.tsv')
     BOWTrain = FileHandlers.LoadFile('../PickleFiles/bagofwordsTrain.pkl')
     BOWTest = FileHandlers.LoadFile('../PickleFiles/bagofwordsTest.pkl')
-    model = imports.RandomForestClassifier(n_estimators=600,random_state=1,n_jobs=-1)
+    model = imports.RandomForestClassifier(n_estimators=600,min_samples_split=3,n_jobs=-1)
     model.fit(BOWTrain[:40000],Train['sentiment'][:40000])
     ScoresBOW = imports.cross_val_score(model, BOWTest[:20000], Test['sentiment'][:20000],scoring='accuracy',cv=10, n_jobs=-1)
-    print(ScoresBOW)
+    print("Scores for the Bag Of Words are: ", ScoresBOW)
     TFIDTrain = FileHandlers.LoadFile('../PickleFiles/TF-IDFTrain.pkl')
     TFIDTest = FileHandlers.LoadFile('../PickleFiles/TF-IDFTest.pkl')
     model.fit(TFIDTrain[:40000],Train['sentiment'][:40000])
